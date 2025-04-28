04a8e623cb96b5ec3a4a5f516aab14978bcad0b8
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-04-28 16:11:02 -0500

    feat: implement Dragonball Super page with character addition and listing

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -15,15 +15,15 @@ interface Character {
   styleUrl: './dragonball.component.css',
 })
 export class DragonballPageComponent {
-  name = signal('Gohan');
-  power = signal(100);
+  name = signal('');
+  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9000 },
-    { id: 2, name: 'Vegeta', power: 8500 },
-    { id: 4, name: 'Piccolo', power: 6000 },
-    { id: 3, name: 'Yamcha', power: 500 },
-    { id: 5, name: 'Frieza', power: 10000 },
+    // { id: 2, name: 'Vegeta', power: 8500 },
+    // { id: 4, name: 'Piccolo', power: 6000 },
+    // { id: 3, name: 'Yamcha', power: 500 },
+    // { id: 5, name: 'Frieza', power: 10000 },
   ]);
 
   powerClasses = computed(() => {
